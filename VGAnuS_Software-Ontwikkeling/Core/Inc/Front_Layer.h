09d91d33392cc30073860bc0499a1206af3a9935
SecVerg <secvergouwen@gmail.com>
2021-04-22 14:52:25 +0200

    Added front layer file

---
+++ VGAnuS_Software-Ontwikkeling/Core/Inc/Front_Layer.h
@@ -0,0 +1,13 @@
+/*
+ * Front_Layer.h
+ *
+ *  Created on: 22 Apr 2021
+ *      Author: secve
+ */
+
+#ifndef INC_FRONT_LAYER_H_
+#define INC_FRONT_LAYER_H_
+
+
+
+#endif /* INC_FRONT_LAYER_H_ */
---
+++ VGAnuS_Software-Ontwikkeling/Core/Src/Front_Layer.c
@@ -0,0 +1,8 @@
+/*
+ * Front_Layer.c
+ *
+ *  Created on: 22 Apr 2021
+ *      Author: secve
+ */
+
+
