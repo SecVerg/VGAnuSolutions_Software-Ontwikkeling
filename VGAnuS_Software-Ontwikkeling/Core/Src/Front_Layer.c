0bc9ae74553b301c11956f504e500a4fa583fe13
SecVerg <secvergouwen@gmail.com>
2021-04-28 11:55:47 +0200

    Updated Front Layer

--- VGAnuS_Software-Ontwikkeling/Core/Src/Front_Layer.c
+++ VGAnuS_Software-Ontwikkeling/Core/Src/Front_Layer.c
@@ -11,12 +11,22 @@ void FL_Parse(char *buf)
 {
 	//char splitUp[100] = buf;  //Array in which the sentence is placed
 	char *woord;
+	char woorden[10][10];
+	int i = 0;
 
 	woord = strtok(buf, ",");	 //Find the first word in the sentence which is seperated via the delimiter ","
+	sprintf(woorden[i], woord);
 
 	while(woord != NULL){					 //Find all the other words with the delimiter "," between them
-		HAL_UART_Transmit_DMA(&huart2, (uint8_t*)woord, strlen(woord));						// send a response
+		sprintf(woorden[i], woord);
+		i++;
 		woord = strtok(NULL, ",");
 		//printf("%s\n", woord);			 //print each word
 	}
+
+	for (i = 0; i < 10; i++)
+	{
+		HAL_UART_Transmit_DMA(&huart2, (uint8_t*)woorden[i], strlen(woorden[i]));						// send a response
+	}
+
 }
