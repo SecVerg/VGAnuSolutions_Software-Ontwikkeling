83ac4b20a9668adf0089ae35a4fd07ec19c16c65
killerpiraat <chris-meerkerk@hotmail.com>
2021-04-28 11:13:55 +0200

    Update Front_Layer.c

--- VGAnuS_Software-Ontwikkeling/Core/Src/Front_Layer.c
+++ VGAnuS_Software-Ontwikkeling/Core/Src/Front_Layer.c
@@ -4,5 +4,21 @@
  *  Created on: 22 Apr 2021
  *      Author: secve
  */
+<<<<<<< Updated upstream
+=======
+#include "main.h"
+>>>>>>> Stashed changes
 
 
+<<<<<<< Updated upstream
+=======
+	char splitUp[100] = UART2_txBuffer;  //Array in which the sentence is placed
+
+	char *woord = strtok(splitUp, ",");	 //Find the first word in the sentence which is seperated via the delimiter ","
+
+	while(woord != 0){					 //Find all the other words with the delimiter "," between them
+		woord = strtok(0, ",");
+		printf("%s\n", woord);			 //print each word
+	}
+}
+>>>>>>> Stashed changes
