cf60a8f6b61f5b0530f4d4e7e2c3f9ef9163c549
SecVerg <secvergouwen@gmail.com>
2021-04-29 14:43:00 +0200

    Updated dev branch

---
+++ VGAnuS_Software-Ontwikkeling/Core/Inc/Front_Layer.h
@@ -0,0 +1,13 @@
+/*
+ * Front_Layer.h
+ *
+ *  Created on: 29 Apr 2021
+ *      Author: secve
+ */
+
+#ifndef INC_FRONT_LAYER_H_
+#define INC_FRONT_LAYER_H_
+
+void FL_Parse(char *buf);
+
+#endif /* INC_FRONT_LAYER_H_ */
---
+++ VGAnuS_Software-Ontwikkeling/Core/Inc/Logic_Layer.h
@@ -0,0 +1,13 @@
+/*
+ * Logic_Layer.h
+ *
+ *  Created on: 29 Apr 2021
+ *      Author: secve
+ */
+
+#ifndef INC_LOGIC_LAYER_H_
+#define INC_LOGIC_LAYER_H_
+
+
+
+#endif /* INC_LOGIC_LAYER_H_ */
