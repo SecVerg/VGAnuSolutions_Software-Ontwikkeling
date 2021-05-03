fc474650d7148a9abaf89f79302d584e32353c5f
killerpiraat <chris-meerkerk@hotmail.com>
2021-05-03 09:16:36 +0200

    Updated

--- VGAnuS_Software-Ontwikkeling/EE-API-library/Src/API_io.c
+++ VGAnuS_Software-Ontwikkeling/EE-API-library/Src/API_io.c
@@ -15,3 +15,31 @@ void API_init_IO(void)
 	// Fill the screen with white
 	UB_VGA_FillScreen(VGA_COL_WHITE);
 }
+
+//bitmap arrays
+byte numberArray[][7] = {
+	// happy smiley
+	{
+		0b0011111000,
+		0b0100000100,
+		0b1000000010,
+		0b1010001010,
+		0b1000000010,
+		0b1010001010,
+		0b1001110010,
+		0b0100000100,
+		0b0011111000,
+	},
+	// sad smiley
+	{
+		0b0011111000,
+		0b0100000100,
+		0b1000000010,
+		0b1010001010,
+		0b1000000010,
+		0b1001110010,
+		0b1010001010,
+		0b0100000100,
+		0b0011111000,
+	},
+};
