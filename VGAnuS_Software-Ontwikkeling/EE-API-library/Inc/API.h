83d2508df335b96c2510080af0081ce3b20ebe27
SecVerg <secvergouwen@gmail.com>
2021-05-05 19:38:42 +0200

    Added Arial font functionality

--- VGAnuS_Software-Ontwikkeling/EE-API-library/Inc/API.h
+++ VGAnuS_Software-Ontwikkeling/EE-API-library/Inc/API.h
@@ -11,14 +11,16 @@
 #include "main.h"
 #include "API_io.h"
 #include "API_draw.h"
-//#include "Consolas.h"
 
+#define BIT_AMT  27
 
-//--------------------------------------------------------------
-// Font struct
-//--------------------------------------------------------------
+#define ARIAL_NORM		0x00
+#define ARIAL_THICC		0x01
+#define ARIAL_CURS		0x02
+#define CONSOLAS_NORM	0x03
+#define CONSOLAS_THICC	0x04
+#define CONSOLAS_CURS	0x05
 
-#define sgn(x) ((x<0)?-1:((x>0)?1:0)) /* macro to return the sign of a
-                                         number */
+#define sgn(x) ((x<0)?-1:((x>0)?1:0)) /* macro to return the sign of a number */
 
 #endif /* INC_API_H_ */
--- VGAnuS_Software-Ontwikkeling/EE-API-library/Inc/API_draw.h
+++ VGAnuS_Software-Ontwikkeling/EE-API-library/Inc/API_draw.h
@@ -13,21 +13,11 @@
 
 #define BIT_AMT  27
 
-typedef struct VGA_Font
-{
-	uint8_t* font_data; 		//
-
-	uint8_t Height;			//
-
-	uint8_t Width;			//
-} VGA_Font;
-
-
 int API_Draw_Line(uint16_t x, uint8_t y, uint16_t x2, uint8_t y2, uint8_t color, uint8_t thicc);
 int API_Draw_Rectangle(uint16_t x, uint8_t y, uint16_t width, uint8_t height, uint8_t color, uint8_t filled);
 int API_Draw_Clearscreen(uint8_t color);
 int API_Draw_Bitmap(uint8_t Number, uint16_t X_pos, uint8_t Y_pos);
 int API_Draw_Text(uint16_t x, uint8_t y, uint8_t color, char* text, char* font, uint8_t fontsize, char* fontstyle);
-int API_Draw_Char (int x, int y, int color, char character, VGA_Font *font);
+int API_Draw_Char (uint8_t x, uint8_t y, uint8_t color, char character, uint8_t font);
 
 #endif /* INC_API_DRAW_H_ */
